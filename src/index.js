98c8c62dbcb5031907fcc0e3c9f3adffb380a851
Sudhanshu Ranjan <perfectsudh@gmail.com>
2019-12-24 08:47:58 +0530

    added webpack config

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-const greet = name => console.log(`Hello, ${name}`)
-greet("Jon Snow")
+import sum from "./sum"
+
+console.log(sum(2,4))
+
+
+// const greet = name => console.log(`Hello, ${name}`)
+// greet("Jon Snow")
